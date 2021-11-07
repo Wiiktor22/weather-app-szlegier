a92042dd3124f2418623f02e862e9e22846fd59c
Wiktor Szlegier <22wiktors@gmail.com>
2021-11-07 10:28:45 +0100

    Add gradient background into main screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+        start={{ x: 0.15, y: 0.2 }}
+        colors={['rgba(130,176,250, 1)', 'rgba(66, 135, 245, 1)']}
+        style={styles.background}
+      >
+        <Text>
+          Weather App
+        </Text>
+      </LinearGradient>
     </View>
   );
 }
@@ -14,7 +21,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+  },
+  background: {
+    flex: 1,
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
